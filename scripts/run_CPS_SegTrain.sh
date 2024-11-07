0076597ea2d28ca2dfe3ee3dcdd42cb94de467bc
phylun <phylun@naver.com>
2024-11-07 18:24:19 +0900

    Update and rename run_CPS_SegTrain.sh to run_SCPS_SegTrain.sh

--- scripts/run_CPS_SegTrain.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-accelerate launch --multi_gpu --main_process_port=29559 accelerate_SingleCPS_SegTrain.py --name CPSInpaintCrack_PoolFormerS36_SD21 --mixed_precision 'bf16' --Snet 'PoolformerS36' --inpainting --with_tracking
