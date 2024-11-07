63d998d6f9d061972c2e3f6cedcda2d4d04da115
phylun <phylun@naver.com>
2024-11-07 21:51:58 +0900

    updated by author

--- options/SCPS_train_options.py
+++ options/SCPS_train_options.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from options.common_options import CommonOptions
-
+ 
 class SCPSTrainOptions(CommonOptions):
     def initialize(self, parser):        
         parser = CommonOptions.initialize(self, parser)        
