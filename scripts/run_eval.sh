2adec2a5287ec3ab7af8f1c839b1f07da6014c67
phylun <phylun@naver.com>
2024-11-07 17:13:51 +0900

    Update run_eval.sh

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # # PoolFormerS36
-# python accelerate_SegTest.py --name CPSInpaintCrack_PoolFormerS36_SD21 --Snet PoolformerS36
-python accelerate_SegTest.py --name CPSInpaintCrack_PoolFormerS36_SD21 --Snet PoolformerS36 --best_epoch 100
+python accelerate_SegTest.py --name CPSInpaintCrack_PoolFormerS36_SD21 --Snet PoolformerS36
+# python accelerate_SegTest.py --name CPSInpaintCrack_PoolFormerS36_SD21 --Snet PoolformerS36 --best_epoch 100
