679a9c85c7af3a1ea14a9e4548e8aace21017d80
phylun <phylun@naver.com>
2024-11-07 21:50:54 +0900

    updated by author

--- scripts/run_SCPS_SegTrain.sh
+++ scripts/run_SCPS_SegTrain.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-accelerate launch --multi_gpu --main_process_port=29559 accelerate_SelfCPS_SegTrain.py --name CPSInpaintCrack_PoolFormerS36_SD21 --mixed_precision 'bf16' --Snet 'PoolformerS36' --inpainting --with_tracking
+accelerate launch --multi_gpu --main_process_port=29559 accelerate_SelfCPS_SegTrain.py --name CPSInpaintCrack_PoolFormerS36_SD21 --mixed_precision 'bf16' --Snet 'PoolformerS36' --inpainting --with_tracking 
